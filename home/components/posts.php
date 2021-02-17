556b4f43f42a363ce278e24c7df8828783d734b0
yosri mhadmi <bavary1515@gmail.com>
2021-02-17 19:58:05 +0100

    rendering dynamic posts.

--- home/components/posts.php
+++ home/components/posts.php
@@ -1,29 +1,21 @@
 <?php
-$query = 'SELECT title, created_at, description ';
+$query = 'SELECT title, created_at, description, image ';
 $query .= 'FROM posts';
 
 $result  = execute($query);
 
-while ($row = mysqli_fetch_assoc($result)) {
-  $title = $row['title'];
+while ($post = mysqli_fetch_assoc($result)) {
+?>
+  <div class="post">
+    <h2><a href="#"><?php echo $post['title'] ?></a></h2>
+    <p class="lead">by <a href="index.php">yosri</a></p>
+    <p>Posted on <?php echo $post['created_at'] ?></p>
+    <hr />
+    <img class="img-responsive" src="/static/images/<?php echo $post['image'] ?>" />
+    <hr />
+    <p><?php echo $post['description'] ?></p>
+    <a class="btn btn-primary" href="#">Read More</a>
+  </div>
+<?php
 }
 ?>
-
-<div class="post">
-  <h2><a href="#"><?php echo $title ?></a></h2>
-  <p class="lead">by <a href="index.php">Start Bootstrap</a></p>
-  <p>
-    <span class="glyphicon glyphicon-time"></span> Posted on August 28,
-    2013 at 10:00 PM
-  </p>
-  <hr />
-  <img class="img-responsive" src="http://placehold.it/900x300" alt="" />
-  <hr />
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore,
-    veritatis, tempora, necessitatibus inventore nisi quam quia repellat
-    ut tempore laborum possimus eum dicta id animi corrupti debitis
-    ipsum officiis rerum.
-  </p>
-  <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-</div>
