8bbb6521619dc9e3b434cea170268bfd4ebd91f9
yosri mhadmi <bavary1515@gmail.com>
2021-02-18 21:54:00 +0100

    improved posts redablity code.

--- home/components/posts/construct_where_clause.php
+++ home/components/posts/construct_where_clause.php
@@ -2,10 +2,12 @@
 <?php
 function construct_where_clause() {
   $keywords = get_keywords_query_param();
+  $keywords = str_replace(',', ' ', $keywords);
+  $keywords = explode(' ', $keywords);
 
   $where = 'WHERE ';
 
-  foreach (explode(' ', $keywords) as $keyword) {
+  foreach ($keywords as $keyword) {
     $where .= " keywords LIKE '%$keyword%' OR ";
   }
 
--- home/components/posts/get_keywords_query_param.php
+++ home/components/posts/get_keywords_query_param.php
@@ -1,8 +1,4 @@
 <?php
 function get_keywords_query_param() {
-  if (!isset($_GET['keywords'])) {
-    return '';
-  }
-
-  return str_replace(',', ' ', $_GET['keywords']);
+  return isset($_GET['keywords']) ? $_GET['keywords'] : '';
 }
