1f800c6f3d9e42bd87c2709dd34ee5c20eeccaec
yosri mhadmi <bavary1515@gmail.com>
2021-02-18 10:27:42 +0100

    printing more categories on the left column.

--- home/components/categories.php
+++ home/components/categories.php
@@ -19,16 +19,16 @@ function print_categories($offset = 0) {
   <div class="row">
     <div class="col-lg-6">
       <ul class="list-unstyled">
-        <?php print_categories(); ?>
-      </ul>
-    </div>
-    <div class="col-lg-6">
-      <ul class="list-unstyled">
         <?php
         $offset = is_float($num_rows / 2) ? 1 : 0;
 
         print_categories($offset);
         ?>
+      </ul>
+    </div>
+    <div class="col-lg-6">
+      <ul class="list-unstyled">
+        <?php print_categories(); ?>
       </ul>
     </div>
   </div>
