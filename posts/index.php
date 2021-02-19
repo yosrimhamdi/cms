fa9c4c6ffd4ea618c89e20798b6bff6ea6cf2582
yosri mhadmi <bavary1515@gmail.com>
2021-02-19 06:49:27 +0100

    improved folder file structure.

--- posts/index.php
+++ posts/index.php
@@ -19,7 +19,7 @@
     <div class="row">
       <div class="col-md-8">
         <h1 class="page-header">Page Heading <small>Secondary Text </small></h1>
-        <?php include "components/posts/posts.php" ?>
+        <?php include "components/posts.php" ?>
         <ul class="pager">
           <li class="previous"><a href="#">&larr; Older</a></li>
           <li class="next"><a href="#">Newer &rarr;</a></li>
