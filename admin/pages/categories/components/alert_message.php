e592335fdd436834effdfd3fa28fc65f8dd561a5
yosri mhadmi <bavary1515@gmail.com>
2021-02-19 20:51:38 +0100

    improved alert message component.

--- admin/pages/categories/components/alert_message.php
+++ admin/pages/categories/components/alert_message.php
@@ -1,11 +1,11 @@
 <?php
-$alert_class = 'success';
+if (isset($message)) {
+  $alert_class = $message === 'added.' ? 'success' : 'danger';
 
-if ($message !== 'added.') {
-  $alert_class = 'danger';
+?>
+  <div class="alert alert-<?php echo $alert_class ?>" role="alert">
+    <?php echo $message ?>
+  </div>
+<?php
 }
 ?>
-
-<div class="alert alert-<?php echo $alert_class ?>" role="alert">
-  <?php echo $message ?>
-</div>
