2e8dc8eb9221cd31af35a17d5e3c0a4c3193878e
yosri mhadmi <bavary1515@gmail.com>
2021-02-28 14:03:59 +0100

    disbled autoformatting.

--- admin/categories/components/form.php
+++ admin/categories/components/form.php
@@ -1,14 +1,14 @@
 <?php
 $url =  $_SERVER['REQUEST_URI'];
 
-$action = 'create';
+$form_action = 'create';
 $button_text = 'Add category';
 
 $category_id = '';
 $category_tite = '';
 
 if (str_contains($url, 'update')) {
-  $action = 'update';
+  $form_action = 'update';
   $button_text = 'Update category';
 
   $category_id = $_GET['update_id'];
@@ -16,13 +16,32 @@ if (str_contains($url, 'update')) {
 }
 ?>
 
-<form action="/admin/categories/actions/<?php echo $action ?>.php" method="post">
+<form 
+  action="/admin/categories/actions/<?php echo $form_action ?>.php" 
+  method="post"
+>
   <div class="form-group">
     <label><?php echo $button_text ?></label>
-    <input type="text" class="form-control" name="title" value="<?php echo $category_tite ?>" autofocus>
-    <input type="text" name="id" value="<?php echo $category_id ?>" hidden>
+    <input 
+      type="text"
+      class="form-control"
+      name="title"
+      value="<?php echo $category_tite ?>" 
+      autofocus
+    >
+    <input 
+      type="text"
+      name="id" 
+      value="<?php echo $category_id ?>" 
+      hidden
+    >
   </div>
   <div class="form-group">
-    <input class="btn btn-primary" type="submit" name="submit" value="<?php echo $button_text ?>">
+    <input 
+      class="btn btn-primary" 
+      type="submit" 
+      name="submit" 
+      value="<?php echo $button_text ?>"
+    >
   </div>
 </form>
