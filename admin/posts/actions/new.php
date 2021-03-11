cef8ac27c466a318a0ac718f445375e04cf1fb7f
yosrimhamdi <bavary1515@gmail.com>
2021-03-11 15:15:05 +0100

    saving images on the server.

--- admin/posts/actions/new.php
+++ admin/posts/actions/new.php
@@ -6,12 +6,15 @@ $category_id = $_POST['category_id'];
 $title = $_POST['title'];
 $keywords = $_POST['keywords'];
 $description = $_POST['description'];
-echo $image = $_FILE['image'];
+$image = $_FILES['image'];
+echo $image_file_name = $image['name'];
+$image_tmp_file = $image['tmp_name'];
 
-$query  = "INSERT INTO posts(category_id, title, keywords, description, approved) ";
-$query .= "VALUES($category_id, '$title', '$keywords', '$description', true)";
+$query  = "INSERT INTO posts(category_id, title, keywords, description, image, approved) ";
+$query .= "VALUES($category_id, '$title', '$keywords', '$description', '$image_file_name', true)";
 
+execute($query);
 
-// execute($query);
+move_uploaded_file($image_tmp_file, "../../../static/images/$image_file_name");
 
-// redirect('/admin/posts');
+redirect('/admin/posts');
