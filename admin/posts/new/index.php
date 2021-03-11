221de0c4b37422ada7d7a4084660ec723f5d71f6
yosrimhamdi <bavary1515@gmail.com>
2021-03-11 14:37:06 +0100

    rendering catgories titles.

--- admin/posts/new/index.php
+++ admin/posts/new/index.php
@@ -1,31 +1,37 @@
 <?php include '../../layouts/top_bun.php' ?>
+<?php include '../../../db/execute.php' ?>
 
 <div class="col-xs-6">
-  <form 
-    action="/admin/posts/actions/new.php"
-    method="post"
-    enctype="multipart/form-data"
-  >
+  <form action="/admin/posts/actions/new.php" method="post" enctype="multipart/form-data">
     <div class="form-group">
       <label for="title">title</label>
       <input type="text" class="form-control" name="title">
     </div>
     <div class="form-group">
-      <label for="post_tags">keywords</label>
-      <input type="text" class="form-control" name="post_tags">
+      <label for="keywords">keywords</label>
+      <input type="text" class="form-control" name="keywords">
     </div>
     <div class="form-group">
-      <label for="post_content">description</label>
-      <textarea class="form-control " name="post_content" cols="30" rows="10">
+      <label for="description">description</label>
+      <textarea class="form-control " name="description" cols="30" rows="10">
       </textarea>
     </div>
-        <div class="form-group">
+    <div class="form-group">
       <label for="category">category</label>
-      <select name="post_category">
+      <select name="category">
+        <?php
+        $query = "SELECT title FROM categories";
+
+        $titles = execute($query);
+
+        while ($title = mysqli_fetch_row($titles)[0]) {
+          echo "<option value='$title'>$title</option>";
+        }
+        ?>
       </select>
     </div>
     <div class="form-group">
-      <label for="post_image">image</label>
+      <label for="image">image</label>
       <input type="file" name="image" accept="image/*">
     </div>
     <div class="form-group">
