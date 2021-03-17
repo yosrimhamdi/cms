cc80fc389ee763f16c91edeaee888ba520404530
yosrimhamdi <bavary1515@gmail.com>
2021-03-17 21:48:41 +0100

    using the build it php email validator.

--- validators/is_email.php
+++ validators/is_email.php
@@ -1,9 +1,5 @@
 <?php
 function is_email($email) {
-
-  $find1 = strpos($email, '@');
-  $find2 = strpos($email, '.');
-
-  return ($find1 !== false && $find2 !== false && $find2 > $find1);
+  return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 ?>
