c36231615ee3f3eeba2fb1adf4ebdeb1d6cb68ee
yosrimhamdi <bavary1515@gmail.com>
2021-03-23 13:57:40 +0100

    added a username.

--- admin/layouts/nav.php
+++ admin/layouts/nav.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
   <div class="navbar-header">
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
@@ -10,10 +12,10 @@
   </div>
   <ul class="nav navbar-right top-nav">
     <li class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['firstname'] . ' ' . $_SESSION['lastname'] ?> <b class="caret"></b></a>
       <ul class="dropdown-menu">
         <li><a href="#"><i class="fa fa-fw fa-user"></i> Profile</a></li>
-        <li><a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a></li>
+        <li><a href="/login/actions/logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a></li>
       </ul>
     </li>
   </ul>
--- login/actions/logout.php
+++ login/actions/logout.php
@@ -1,4 +1,5 @@
 <?php include_once '../../functions/redirect.php' ?>
+<?php include_once '../../functions/redirect_if_not_legal.php' ?>
 
 <?php
 session_start();
