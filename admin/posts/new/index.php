c7b125fe498759d35782528e79e1035c2c96b4d0
yosrimhamdi <bavary1515@gmail.com>
2021-03-26 09:54:00 +0100

    added a text editor in post creation.

--- admin/posts/new/index.php
+++ admin/posts/new/index.php
@@ -13,7 +13,7 @@
     </div>
     <div class="form-group">
       <label for="description">description</label>
-      <textarea class="form-control " name="description" cols="30" rows="10"></textarea>
+      <textarea class="form-control " id="editor" name="description" cols="30" rows="10"></textarea>
     </div>
     <div class="form-group">
       <label for="category">category</label>
@@ -30,4 +30,15 @@
     </div>
   </form>
 </div>
+<script src="https://cdn.ckeditor.com/ckeditor5/27.0.0/classic/ckeditor.js"></script>
+<script>
+ClassicEditor
+  .create( document.querySelector( '#editor' ) )
+  .then( editor => {
+      console.log( editor );
+  } )
+  .catch( error => {
+      console.error( error );
+  } );
+</script>
 <?php include_once '../../layouts/bottom_bun.php' ?>
