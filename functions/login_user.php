9234a49786bfbd56fba758c433889d916f81d809
yosrimhamdi <bavary1515@gmail.com>
2021-03-30 22:59:44 +0100

    returning a cookie for logging in.

--- functions/login_user.php
+++ functions/login_user.php
@@ -3,7 +3,7 @@
 <?php
 function login_user($user) {
   $jwt  = "firstname=$user[firstname],lastname=$user[lastname],";
-  $jwt .= "role=$user[role]";
+  $jwt .= "role=$user[role],";
   $jwt .= "image=$user[image]";
 
   setcookie('jwt', $jwt, time() + 3600 * 24 * 2);
