b7127947999765eeba6d22a7e60ebf050ded4d84
yosrimhamdi <bavary1515@gmail.com>
2021-03-31 22:41:14 +0100

    reformed redirect_if_logged_in functions.

--- functions/redirect/redirect_if_logged_in.php
+++ functions/redirect/redirect_if_logged_in.php
@@ -1,8 +1,12 @@
 <?php include_once '../functions/redirect/redirect.php' ?>
+<?php include_once '../login/functions/is_logged_in.php' ?>
+<?php include_once '../login/functions/is_admin.php' ?>
 
 <?php
 function redirect_if_logged_in() {
-   if(isset($_SESSION['role'])) {
+   if(is_logged_in() && is_admin()) {
+    redirect('/admin');
+  } else if (is_logged_in() && !is_admin()) {
     redirect('/');
   }
 }
