f233824bd831420ced80a3abcc5a0cfcb12bb3dc
yosrimhamdi <bavary1515@gmail.com>
2021-03-31 22:46:21 +0100

    removing the jwt when logged out.

--- login/actions/logout.php
+++ login/actions/logout.php
@@ -2,10 +2,6 @@
 <?php include_once '../../functions/redirect/redirect_if_not_legal.php' ?>
 
 <?php
-session_start();
-
-unset($_SESSION['firstname']);
-unset($_SESSION['lastname']);
-unset($_SESSION['role']);
+setcookie('jwt', null, -1, '/');
 
 redirect('/login');
